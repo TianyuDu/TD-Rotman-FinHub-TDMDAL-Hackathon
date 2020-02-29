17744e0bd66618607f8374c93e05834d6dbd87d5
tianyudu <masterdu9951@icloud.com>
2020-02-29 02:10:09 -0500

    update

--- data_proc/gen_LMD_score.py
+++ data_proc/gen_LMD_score.py
@@ -1,5 +1,5 @@
 """
-
+Generate sentiment scores using 
 """
 import argparse
 from compute_score_LMD import generate_dataset
@@ -8,4 +8,6 @@ from compute_score_LMD import generate_dataset
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--l", type=str)
-    args = 
+    args = parser.parse_args()
+    letter = args.l.upper()
+    df = generate_dataset(letter_starts=letter)
