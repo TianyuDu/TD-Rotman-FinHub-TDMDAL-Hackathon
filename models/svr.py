2a23e6fa1641f69cc889c2efececdba2bc4f3175
tianyudu <masterdu9951@icloud.com>
2020-02-29 11:53:54 -0500

    update

---
+++ models/svr.py
@@ -0,0 +1,12 @@
+"""
+Baseline Support Vector Regressor.
+"""
+import numpy as np
+import pandas as np
+
+from sklearn import svm
+
+from 
+
+
+def main() >
--- models/training.py
+++ models/training.py
@@ -9,29 +9,18 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 
-from data_feed import regression_feed
-
 from sklearn import metrics
-
-
-def check_data(data) -> None:
-    """
-    Validate dataset input.
-    """
-    raise NotImplementedError
 
 
 def training_pipeline(
     model,
     data: List[np.ndarray],
-    task: Union["regression", "classification"],
     num_fold: int = 1,
 ) -> dict():
     """
     Train and evaluate model.
     """
     # ==== Check Arguments ====
-    assert task in ["regression", "classification"], "Invalid task."
     val_perf_lst = {
         "loss": list(),
         "directional_accuracy": list()
@@ -42,10 +31,10 @@ def training_pipeline(
     }
     val_perf = dict()
     # ==== Data Preprocessing ====
-    X_train_raw, y_train_raw, X_test, y_test = data
-    check_data(data)
+    X_train_raw, y_train_raw = data
     val_loss_fold = list()
     val_diracc_fold = list()
+    # ==== N-Fold ====
     for _ in range(num_fold):
         # Train validation split.
         # Note that the order of dataset returned by
