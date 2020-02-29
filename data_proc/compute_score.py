b1103f25e69b7338fb06ba648a2ca34c42b8568e
tianyudu <masterdu9951@icloud.com>
2020-02-28 21:05:56 -0500

    update

--- data_proc/compute_score.py
+++ data_proc/compute_score.py
@@ -4,6 +4,17 @@ Computes scores.
 import numpy as np
 import pandas as pd
 
+from datetime import datetime
+
+
+lmd_path = "../sentiment_data/LoughranMcDonald_SentimentWordLists_2018.xlsx"
+
+LMD = pd.read_excel(
+    lmd_path,
+    sheet_name=["Negative", ],
+    header=False
+)
+
 
 def get_transcript_LMD_score(
     body: str
