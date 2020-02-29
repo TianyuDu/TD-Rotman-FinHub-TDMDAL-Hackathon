752cf5e925a8fc9cdfddea43519d448e4a044f1a
tianyudu <masterdu9951@icloud.com>
2020-02-28 21:11:48 -0500

    update

--- data_proc/proc_json.py
+++ data_proc/proc_json.py
@@ -1,13 +1,12 @@
 """
 Process json files.
 """
+import json
+from datetime import datetime
 from typing import List
 
 import numpy as np
 import pandas as pd
-
-import json
-
 
 TRANSCRIPT_DIR = "../hackathon_data/company_transcripts/"
 
@@ -20,3 +19,7 @@ def load_individual_transcript(
     with open(path, "r") as f:
         data = json.load(f)
     return data
+
+
+def convert_time(s: str) -> datetime:
+    return datetime.fromtimestamp(s / 1000).strftime("%Y-%m-%d %H:%M:%S.%f")
