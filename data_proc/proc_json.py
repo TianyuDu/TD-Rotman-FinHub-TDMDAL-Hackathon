014aa0bd35975b1f0ed1114a9c5b9ce559214867
tianyudu <masterdu9951@icloud.com>
2020-02-28 20:40:49 -0500

    add file

--- data_proc/proc_json.py
+++ data_proc/proc_json.py
@@ -1,2 +1,21 @@
 """
+Process json files.
 """
+from typing import List
+
+import numpy as np
+import pandas as pd
+
+import json
+
+
+TRANSCRIPT_DIR = "../hackathon_data/company_transcripts/"
+
+
+def load_individual_transcript(
+    company: str,
+    subset: List[str]
+) -> dict:
+    path = TRANSCRIPT_DIR + company + ".json"
+    with open(path, "r") as f:
+        data = json.load(f)
