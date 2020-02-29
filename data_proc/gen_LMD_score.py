3d506c360885e4098f01ffd1e0ae69dd3d8295e7
tianyudu <masterdu9951@icloud.com>
2020-02-29 00:31:02 -0500

    update

---
+++ data_proc/gen_LMD_score.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+import argparse
+from compute_score_LMD import generate_dataset
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--l", type=str)
+    args = 
