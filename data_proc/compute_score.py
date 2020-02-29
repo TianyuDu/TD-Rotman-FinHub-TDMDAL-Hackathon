aa48053107db5245a96003571d83722a311b49f2
tianyudu <masterdu9951@icloud.com>
2020-02-28 20:54:05 -0500

    update

---
+++ data_proc/compute_score.py
@@ -0,0 +1,14 @@
+"""
+Computes scores.
+"""
+import numpy as np
+import pandas as pd
+
+
+def get_transcript_LMD_score(
+    body: str
+) -> np.ndarray:
+    """
+    Compute sentiment
+    """
+    raise NotImplementedError
--- data_proc/proc_json.py
+++ data_proc/proc_json.py
@@ -14,8 +14,9 @@ TRANSCRIPT_DIR = "../hackathon_data/company_transcripts/"
 
 def load_individual_transcript(
     company: str,
-    subset: List[str]
+    subset: List[str] = None
 ) -> dict:
     path = TRANSCRIPT_DIR + company + ".json"
     with open(path, "r") as f:
         data = json.load(f)
+    return data
